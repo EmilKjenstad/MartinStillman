b0aa0dc786d6134740301a6faf948987bbaf7af3
Emil Kjenstad <emil.kjenstad@gmail.com>
2023-10-31 15:07:14 -0500

    Code Cleanup + Carusel

--- static/js/script.js
+++ static/js/script.js
@@ -1,27 +1,9 @@
-function chbg(img) {
-  document.getElementById("thisTest").src = "./static/assets/img/"+img+".png";
-}
-
-function showContent(selector) {
-  var toggleClass = 'card-body-content-active';
-  var location = document.getElementById(selector);
-  var hasClass = location.classList.contains(toggleClass);
-
-  if ( !hasClass ) {
-    document.getElementById("btn_"+selector).innerHTML = "Show less";
-    location.classList.add(toggleClass);
-  } 
-  else {
-    location.classList.remove(toggleClass);
-    document.getElementById("btn_"+selector).innerHTML = "Show more";
-  }
-}
 
 //load when scroll to it?
 window.onload = function() {
   //populateBio();
-  populateService();
-  populateContact();
+  //populateService();
+  //populateContact();
 }
 
 function populateContact() {
